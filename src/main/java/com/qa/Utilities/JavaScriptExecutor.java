20642634760bdb6282e1b89d90b43c1c17b42073
Anilkumar2127 <cyberattack554@gmail.com>
2025-07-19 20:28:13 +0530

    Adding js and webelement utilities

---
+++ src/main/java/com/qa/Utilities/JavaScriptExecutor.java
@@ -0,0 +1,93 @@
+package com.qa.Utilities;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class JavaScriptExecutor {
+
+	private WebDriver driver;
+	private JavascriptExecutor js;
+
+	public JavaScriptExecutor(WebDriver driver) {
+		this.driver = driver;
+		this.js = (JavascriptExecutor) this.driver;
+	}
+
+	private void changeColor(String color, WebElement ele) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].style.backgroundColor ='" + color + "'", ele);
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getTitleByJavaScriptExecutor() {
+		return js.executeScript("return document.title").toString();
+	}
+
+	public String getUrlByJavaScriptExecutor() {
+		return js.executeScript("return document.url").toString();
+	}
+
+	public void navigatebackByJavaScriptExecutor() {
+		js.executeScript("history.go(-1)");
+	}
+
+	public void navigateForwardByJavaScriptExecutor() {
+		js.executeScript("history.go(1)");
+	}
+
+	public void refreshBrowserByJavaScriptExecutor() {
+		js.executeScript("history.go(0)");
+	}
+
+	public String getInnerTextByjavaScriptExecutor() {// Usecase : To capture full page test .
+		return js.executeScript("return document.documentElement.innerText").toString();
+	}
+
+	public void scrollPageDownByJavaScriptExecutor() {
+		js.executeScript("0,window.scrollTo(document.body.scrollHeight)");
+	}
+
+	public void scrollPageUpByJavaScriptExecutor() {
+		js.executeScript("window.scrollTo(document.body.scrollHeight),0");
+	}
+
+	public void scrollToElementByJavaScriptExecutor(WebElement element) {
+		js.executeScript("argument[0].scrollIntoView(true)", element);
+	}
+
+	public void drawBorderElementByJavaScriptExecute(WebElement element) {
+		js.executeScript("argument[0].style.border='3px solid red'", element);
+	}
+
+	public void hightlightElementByJavaScriptExecutor(WebElement element) {
+		String bgColor = element.getCssValue("background-color");
+		String wishColor = "rgb(255, 255, 0)";
+		for (int i = 0; i < 10; i++) {
+			changeColor(wishColor, element);
+			changeColor(bgColor, element);
+		}
+	}
+
+	public void zoomInPageByJavaScriptExecutorInChromeEdge(String zoomPercentage)// responsive testing
+	{
+		String zoom="document.body.style.zoom='" + zoomPercentage + "'" + "%";
+		js.executeScript(zoom);
+	}
+	
+	public void zoomOutPageByJavaScriptExecutorInFireFox(String zoomPercentage) {
+		String zoom="document.body.style.MozTransform='" + zoomPercentage + "'" + "%";
+		js.executeScript(zoom);
+	}
+	
+	public void clickOnElementByJavaScriptExecutor(WebElement element) {//Last option if nothing works with webelement,action click then only applicable
+		js.executeScript("arguments[0].click();",element);
+	}
+
+}
--- src/main/java/com/qa/Utilities/WebElementUtiles.java
+++ src/main/java/com/qa/Utilities/WebElementUtiles.java
@@ -7,11 +7,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.ui.Select;
 
 import com.qa.Exceptions.ElementExceptions;
 
 public class WebElementUtiles {
 	private WebDriver driver;
+	private Actions act = null;
 
 	private void nullcheck(CharSequence... value) {// CharSequence is an interface but implements by
 													// stringbuilder,stringbuffer and string classes
@@ -22,25 +26,86 @@ public class WebElementUtiles {
 
 	public WebElementUtiles(WebDriver driver) {
 		this.driver = driver;
-
+		act = new Actions(driver);
 	}
+
+	/****************************
+	 * WebElement based on string
+	 ****************************/
+
+	public By getBy(String locatorType, String valueLocator) {
+		By locator = null;
+		switch (locatorType.toLowerCase().trim()) {
+		case "id":
+			locator = By.id(valueLocator);
+			break;
+		case "name":
+			locator = By.name(valueLocator);
+			break;
+		case "tag":
+			locator = By.tagName(valueLocator);
+			break;
+		case "css":
+			locator = By.cssSelector(valueLocator);
+			break;
+		case "classname":
+			locator = By.className(valueLocator);
+			break;
+		case "linktext":
+			locator = By.linkText(valueLocator);
+			break;
+		case "partiallinktext":
+			locator = By.partialLinkText(valueLocator);
+			break;
+		case "xpath":
+			locator = By.xpath(valueLocator);
+			break;
+		default:
+			System.out.println("plz pass the right locator type: " + locatorType);
+			break;
+		}
+		return locator;
+	}
+
+	public WebElement getElementLocator(String locatorType, String locatorValue) {
+		return driver.findElement(getBy(locatorType, locatorValue));
+	}
+
+	/******************************
+	 * WebElement to get Element
+	 ******************************/
 
 	public WebElement getElementLocator(By locator) {
 		return driver.findElement(locator);
 	}
 
-	/*******************************
-	 * WebLements to getElements
+	/********************************
+	 * WebElments to getElements
 	 ********************************/
+	/*
+	 * Description:It returns multiple element locators
+	 * 
+	 * @Params:Expecting locator
+	 */
 	public List<WebElement> getElementLocators(By locator) {
 		return driver.findElements(locator);
 	}
 
-	public int getElementCount(By locator) {
+	/*
+	 * Description:It returns total count of elements
+	 * 
+	 * @Params:Expecting locator
+	 */
+	public int getElementsCount(By locator) {
 		int size = getElementLocators(locator).size();
 		return size;
 	}
 
+	/*
+	 * Description:It returns list of text contents from multiple elements
+	 * 
+	 * @Params:Expecting locator
+	 */
 	public List<String> getTextFromElements(By locator) {
 		List<WebElement> listOfElements = getElementLocators(locator);
 		List<String> textList = new ArrayList<String>();
@@ -54,7 +119,50 @@ public class WebElementUtiles {
 		return textList;
 	}
 
-	////////////////////////////////////////////////////////////////////////////////
+	/*
+	 * Description: Returns boolean value if only one element is displayed on AUT
+	 * 
+	 * @param:locators
+	 */
+	public boolean checkElementDisplayed(By locator) {
+		if (getElementLocators(locator).size() == 1) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Description: Returns boolean value if you expected elements count is
+	 * displayed on AUT
+	 * 
+	 * @param:locators, total expected elements count.
+	 * 
+	 */
+	public boolean checkElementDisplayed(By locator, int expectedElementCount) {
+		if (getElementLocators(locator).size() == expectedElementCount) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * Description:Clicks on specific element from multiple locators by passing
+	 * expectedValue
+	 * 
+	 * @param:By locator and ecpectedElementtext to be clicked
+	 */
+	public void clickOnSpecificElement(By locator, String expValue) {
+		List<WebElement> listofElementslocators = getElementLocators(locator);
+		for (WebElement ele : listofElementslocators) {
+			String textvalue = ele.getText();
+			if (textvalue.contains(expValue)) {
+				ele.click();
+				break;
+			}
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////
 	public void fillTextBox(By locator, String value) {
 		nullcheck(value);
 		getElementLocator(locator).sendKeys(value);
@@ -74,11 +182,10 @@ public class WebElementUtiles {
 			if (getElementLocator(locator).isDisplayed()) {
 				return true;
 			}
-		}
-		catch (NoSuchElementException e) {
+		} catch (NoSuchElementException e) {
 			e.printStackTrace();
 			System.out.println("=== element Not found ===");
-		}	
+		}
 		return false;
 	}
 
@@ -97,5 +204,139 @@ public class WebElementUtiles {
 	public String getElmentDomPropertyValue(By locator, String propertyName) {
 		return getElementLocator(locator).getDomProperty(propertyName);
 	}
+	////////////////////////////////////////////////////////////////////////////
+	////////////// SELECT KIND DROP DOWN ELEMENTUTILS///////////////////////////
+	////////////////////////////////////////////////////////////////////////////
 
+	/*
+	 * Selecting the element from drop down based on dropdown text
+	 * 
+	 * @param:By locator,String text
+	 */
+	public boolean selectByVisibleTextFromDropDown(By locator, String value) {
+		Select dropDown = new Select(getElementLocator(locator));
+		try {// NoSuchElementException arises due text value is not present in drop down
+			dropDown.selectByVisibleText(value);
+			return true;
+		} catch (NoSuchElementException e) {
+			System.out.println("Text not present in drop down " + value);
+			return false;
+		}
+	}
+
+	/*
+	 * Selecting the element from drop down based on dropdown value
+	 * 
+	 * @param:By locator,String value
+	 */
+	public boolean selectByValueFromDropDown(By locator, String value) {
+		Select dropDown = new Select(getElementLocator(locator));
+		try {
+			dropDown.selectByValue(value);
+			return true;
+		} catch (NoSuchElementException e) {
+			System.out.println("Value not present in dropdown " + value);
+			return false;
+		}
+	}
+
+	/*
+	 * Selecting the element from drop down based on dropdown index
+	 * 
+	 * @param:By locator,String index
+	 */
+	public boolean selectByIndexFromDropDown(By locator, int index) {
+		Select dropDown = new Select(getElementLocator(locator));
+		try {
+			dropDown.selectByIndex(index);
+			return true;
+		} catch (NoSuchElementException e) {
+			System.out.println("No index is selected from dropdown " + index);
+
+			return false;
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////// RELATIVE LOCATOR INTRO AFTER
+	////////////////////////////////////////////////////////////////////////////////////////// 4.x//////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////
+	public String getLeftElementTextFromBaseLocator(By targetLocator, By referenceLocator) {
+		return driver.findElement(RelativeLocator.with(targetLocator).toLeftOf(referenceLocator)).getText();
+	}
+
+	public String getRightElementTextFromBaseLocator(By targetLocator, By referenceLocator) {
+		return driver.findElement(RelativeLocator.with(targetLocator).toLeftOf(referenceLocator)).getText();
+	}
+
+	public String getNearByElementTextFromBaseLocator(By targetLocator, By referenceLocator) {
+		return driver.findElement(RelativeLocator.with(targetLocator).near(referenceLocator)).getText();
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////// Drop Down WithOut Select
+	////////////////////////////////////////////////////////////////////////////////////////////// tag////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * Description: Selecting the drop down values based on condition
+	 * 
+	 * @choiceField :Provide the dropdown field locator to click
+	 * 
+	 * @choicesList :Provide the Lists locators from the drop down field
+	 * 
+	 * @choicesvalue:Give the values to select the choices i.e to select all give as
+	 * "all"
+	 * 
+	 */
+	public void selectChoice(By choiceField, By choicesList, String... choicesvalue) {
+		clickOnElement(choiceField);
+		List<WebElement> choicesLists = getElementLocators(choicesList);
+		if (choicesvalue[0].trim().equalsIgnoreCase("all")) {
+			for (WebElement ele : choicesLists) {
+				ele.click();
+			}
+		} else {
+			for (WebElement ele : choicesLists) {
+				String choiceText = ele.getText();
+				for (String selChoice : choicesvalue) {
+					if (choiceText.equalsIgnoreCase(selChoice)) {
+						ele.click();
+						break;
+					}
+				}
+			}
+		}
+
+	}
+
+	/*
+	 * //////////////////////Actions class elements///////////////////////////////////////////
+	 */
+	
+	public void doActionClick(By locator) {
+		act.click(getElementLocator(locator)).build().perform();
+	}
+
+	public void doSendKeysAction(By locator,String value) {
+		act.sendKeys(value).build().perform();
+	}
+	/*		INTERVIEW QUESTION
+	 * Entering the string value one by one in text field using Actions class
+	 * @Params=Locator
+	 * @Params=Value to enter
+	 * @Params=Pause time 
+	 */
+	public void doSendKeysWithPause(By locator,String value,int pauseTime) {
+		char val[]=value.toCharArray();
+		for(char v:val) {
+			act.sendKeys(getElementLocator(locator), String.valueOf(v))
+				.pause(pauseTime)
+					.build().perform();
+		}
+	}
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	
